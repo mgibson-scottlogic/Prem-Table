19bcaeb931e1220707f693248b5d28005d352a16
MatthewG375 <41404559+MatthewG375@users.noreply.github.com>
2025-01-19 19:32:46 +0000

    Refactoring to follow PEP8 guidelines

--- tables_auto.py
+++ tables_auto.py
@@ -1,3 +1,5 @@
+"""Code to generate a bar graph of English Premier League teams who can still qualify for Europe"""
+
 # importing package
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -7,52 +9,61 @@ import pandas as pd
 
 pd.set_option('display.max_columns', None)
 
-# base url for all FPL API endpoints
-base_url = 'https://fantasy.premierleague.com/api/'
+
+def generate_data():
+    """A function to generate the 'teams' and 'df2' dataframes"""
+    # base url for all FPL API endpoints
+    BASE_URL = 'https://fantasy.premierleague.com/api/'
 
 
-# get data from fixtures endpoint
-fix = requests.get(base_url+'fixtures/', timeout=10).json()
+    # get data from fixtures endpoint
+    fix = requests.get(BASE_URL+'fixtures/', timeout=10).json()
 
 
-# create fixtures dataframe
-fixtures = pd.json_normalize(fix)
+    # create fixtures dataframe
+    fixtures = pd.json_normalize(fix)
 
 
-# get data from bootstrap-static endpoint
-r = requests.get(base_url+'bootstrap-static/', timeout=10).json()
+    # get data from bootstrap-static endpoint
+    r = requests.get(BASE_URL+'bootstrap-static/', timeout=10).json()
 
 
-# create teams dataframe
-teams = pd.json_normalize(r['teams'])
+    # create teams dataframe
+    teams = pd.json_normalize(r['teams'])
 
-# add team colours to dataframe
-team_colours = ['#e20814', '#91beea', '#ca0b17', '#b60501', '#004a9b', '#021581', '#004b97', '#024593', '#282624', '#0c3e94', '#13428d', '#b30011', '#b1d4fa', '#dc1116', '#0d0805', '#f4031c', '#d20911', '#152055', '#7d2d3f', '#feb906']
-teams['colours'] = team_colours
-
-
-# join fixtures to teams
-df = pd.merge(
-    left=fixtures,
-    right=teams,
-    left_on='team_a',
-    right_on='id'
-)
-
-df2 = pd.merge(
-    left=df,
-    right=teams,
-    left_on='team_h',
-    right_on='id'
-)
+    # add team colours to dataframe
+    team_colours = ['#e20814', '#91beea', '#ca0b17', '#b60501', '#004a9b',
+                    '#021581', '#004b97', '#024593', '#282624', '#0c3e94',
+                    '#13428d', '#b30011', '#b1d4fa', '#dc1116', '#0d0805',
+                    '#f4031c', '#d20911', '#152055', '#7d2d3f', '#feb906']
+    teams['colours'] = team_colours
 
 
-def get_team_record(team_id):
-    team_name = teams.loc[teams.id == team_id, 'name'].values[0]
+    # join fixtures to teams
+    df = pd.merge(
+        left=fixtures,
+        right=teams,
+        left_on='team_a',
+        right_on='id'
+    )
+
+    df2 = pd.merge(
+        left=df,
+        right=teams,
+        left_on='team_h',
+        right_on='id'
+    )
+
+    return teams, df2
+
+
+def get_team_record(team_id, df2):
+    """Function to get teams record from their ID"""
+    # team_name = teams.loc[teams.id == team_id, 'name'].values[0]
 
     record_df = df2[ (df2['team_h'] == team_id) | (df2['team_a'] == team_id) ]
     record_df = record_df.reset_index()
-    #record_df[['name_y','team_h_score', 'team_a_score', 'name_x']].head(11)
+    # record_df[['name_y','team_h_score', 'team_a_score', 'name_x']].head(11)
 
     w = 0
     d = 0
@@ -101,25 +112,27 @@ def get_team_record(team_id):
     else:
         gd = str(gd)
 
-    
-    remaining_df = df2[ ((df2['finished'] == False) & (df2['finished_provisional'] == False)) & ((df2['team_h'] == team_id) | (df2['team_a'] == team_id)) ]
+    # fixture is in the future if it has not: Finished, Provisionally Finished, or Started
+    remaining_df = df2[ ((df2['finished'] == False) & (df2['finished_provisional'] == False) & (df2['started'] == False)) &
+                        ((df2['team_h'] == team_id) | (df2['team_a'] == team_id)) ]
     remaining_df = remaining_df.reset_index()
+
+    pts = int((w*3)+(d))
 
     # when testing smaller, - amount of removed fixtures here
     left = int(len(remaining_df))
-    
-    pts = int((w*3)+(d))
-    ply = int(w+d+l)
+    max_theory = int(pts + 3*left)
 
-    max = int(pts + 3*left)
+    # ply = int(w+d+l)
+    # print(f"{team_name} Record: Played: {ply}, Won: {w}, Draw: {d}, Loss: {l}, Points: {pts}, Goal Difference: {gd}, Remaining: {left}")
 
-    #print(f"{team_name} Record: Played: {ply}, Won: {w}, Draw: {d}, Loss: {l}, Points: {pts}, Goal Difference: {gd}, Remaining: {left}")
-
-    return pts, ply, left, max, gd, gf
+    return pts, max_theory, gd, gf
 
 
-def get_remaining_fixtures(team_id):
-    filtered_df = df2[ ((df2['finished'] == False) & (df2['finished_provisional'] == False)) & ((df2['team_h'] == team_id) | (df2['team_a'] == team_id)) ]
+def get_remaining_fixtures(team_id, df2):
+    """Takes a team ID and returns a pandas dataframe of remaining fixtures."""
+    filtered_df = df2[ ((df2['finished'] == False) & (df2['finished_provisional'] == False) & (df2['started'] == False)) &
+                       ((df2['team_h'] == team_id) | (df2['team_a'] == team_id)) ]
     filtered_df = filtered_df.reset_index()
     #filtered_df[['name_y','team_h_score', 'team_a_score', 'name_x']]
 
@@ -129,19 +142,19 @@ def get_remaining_fixtures(team_id):
     opp_name = []
     diff = []
 
-    for index, row in filtered_df.iterrows():
-        if row['team_h'] == team_id:
+    for row in filtered_df.itertuples():
+        if row.team_h == team_id:
             location = "H"
-            oppos = row['team_a']
-            tname = row['name_x']
-            opp_diff = row['team_h_difficulty']
+            oppos = row.team_a
+            tname = row.name_x
+            opp_diff = row.team_h_difficulty
         else:
             location = "A"
-            oppos = row['team_h']
-            tname = row['name_y']
-            opp_diff = row['team_a_difficulty']
+            oppos = row.team_h
+            tname = row.name_y
+            opp_diff = row.team_a_difficulty
 
-        date = row['kickoff_time'][5:10]
+        date = row.kickoff_time[5:10]
         left, right = date.split('-')
         newdate = f"{location} {right}-{left}"
         # print(newdate)
@@ -167,9 +180,15 @@ def get_remaining_fixtures(team_id):
     return filtered_df
 
 
-def generate_table(pos_one, pos_two):
+def generate_table(pos_one=1, pos_two=20):
+    """Function to generate the visualization of the table
+    
+    Keyword Arguments:
+    pos_one -- the first position in the table to show on the image (default 1)
+    pos_two -- the second position in the table to show on the image (default 20)
+    """
     # use the global teams variable
-    global teams
+    teams, df2 = generate_data()
 
 
     # add max points row, goal difference, and goals scored to dataframe (for H2H tiebreakers)
@@ -177,15 +196,16 @@ def generate_table(pos_one, pos_two):
     gd = []
     gf = []
     for team in teams.itertuples():
-        points, played, remaining, max, goal_difference, goals_for = get_team_record(team.id)
-        max_points.append(max)
+        points, max_pts, goal_difference, goals_for = get_team_record(team.id, df2)
+        max_points.append(max_pts)
         gd.append(int(goal_difference))
         gf.append(goals_for)
 
     teams['max_points'] = max_points
     teams['goal_difference'] = gd
     teams['goals_for'] = gf
-    teams = teams.sort_values(by=['max_points', 'goal_difference', 'goals_for'], ascending=[False, False, False])
+    teams = teams.sort_values(by=['max_points', 'goal_difference', 'goals_for'],
+                              ascending=[False, False, False])
 
 
     # make dataframe with all teams in, in order to remove extraneous teams from main dataset
@@ -246,23 +266,23 @@ def generate_table(pos_one, pos_two):
 
     # loop for every team that needs a bar
     for row in teams.itertuples():
-        points, played, remaining, max, goal_difference, goals_for = get_team_record(row.id)
+        points, max_pts, goal_difference, goals_for = get_team_record(row.id, df2)
         goal_difference = f'GD {goal_difference}'
 
         # update lowest theoretical points total if new teams is lower
-        if theory_min > points:
-            theory_min = points
+        theory_min = min(theory_min, points)
 
         # create bar for current points, with team colour
         cpts = plt.bar(row.name, points, color=row.colours, edgecolor=row.colours, width=barwidth)
         bot = points
 
         # loop for every remaining fixture for current team: row['id]
-        fixtures_remaining = get_remaining_fixtures(row.id)
+        fixtures_remaining = get_remaining_fixtures(row.id, df2)
         for fx in fixtures_remaining.itertuples():
 
             dif_col = colours[fx.opposition_difficulty]
-            cur_bar = plt.bar(row.name, 3, bottom=bot, color=dif_col, edgecolor="#808080", lw=1.5, width=barwidth)
+            cur_bar = plt.bar(row.name, 3, bottom=bot,
+                              color=dif_col, edgecolor="#808080", lw=1.5, width=barwidth)
             perma_y = points
 
             # positioning of image and text in upcoming fixture bar
@@ -278,11 +298,15 @@ def generate_table(pos_one, pos_two):
                 # plot the team logo and the fixture date
                 imdis =  team_crest[fx.opposition_id]
                 plt.imshow(imdis, extent=[xleft, xright, ybot, ytop], aspect='auto', zorder=2)
-                
-                plt.text(x+w/2, y+0.18, fx.remaining_fixtures, ha='center', fontname='sans-serif', c="#757171", weight='semibold', size='x-small')
-            
+
+                plt.text(x+w/2, y+0.18, fx.remaining_fixtures,
+                         ha='center', fontname='sans-serif', c="#757171",
+                         weight='semibold', size='x-small')
+
             # goal difference label
-            plt.text(x+w/2, perma_y-0.5, goal_difference, ha='center', fontname='sans-serif', c='white', weight='semibold', size='x-small')
+            plt.text(x+w/2, perma_y-0.5, goal_difference,
+                     ha='center', fontname='sans-serif', c='white',
+                     weight='semibold', size='x-small')
 
             # increment counter by 3, as each fixture has a possible value of 3 points
             bot += 3
@@ -292,7 +316,8 @@ def generate_table(pos_one, pos_two):
             x,y = bar_a.get_xy()
             w, h = bar_a.get_width(), bar_a.get_height()
             #print(x,y,w,h)
-            plt.bar(x+w/2, color=bar_a.get_facecolor(), lw=1.5, height=h+0.01, edgecolor=row.colours, width=barwidth)
+            plt.bar(x+w/2, color=bar_a.get_facecolor(),
+                    lw=1.5, height=h+0.01, edgecolor=row.colours, width=barwidth)
 
     # axis is slightly shorter than the number of teams being plotted
     ax_width = len(teams.index) - 0.5
@@ -317,22 +342,29 @@ def generate_table(pos_one, pos_two):
 
     # format position numbers
     title_pos = [remove_from_top+1, 20 - remove_from_bottom]
-    for x in range(len(title_pos)):
-        if title_pos[x] == 1:
-            title_pos[x] = '1st'
-        elif title_pos[x] == 2:
-            title_pos[x] = '2nd'
-        elif title_pos[x] == 3:
-            title_pos[x] = '3rd'
+    for i, x in enumerate(title_pos):
+        if x == 1:
+            title_pos[i] = '1st'
+        elif x == 2:
+            title_pos[i] = '2nd'
+        elif x == 3:
+            title_pos[i] = '3rd'
         else:
-            title_pos[x] = f'{title_pos[x]}th'
+            title_pos[i] = f'{title_pos[i]}th'
 
 
     # set axis lables and title
     cur_day = datetime.today().strftime('%d-%m-%y')
-    plt.title(f"EPL: The race for European Competitions \n{title_pos[0]} to {title_pos[1]} as of {cur_day}", size=17, fontname='sans-serif', weight='semibold')
-    plt.xlabel("Teams in order of highest possible points total", labelpad=15, size=17, fontname='sans-serif', weight='semibold')
-    plt.ylabel("Points and remaining fixures in chronological order", labelpad=30, size=17, fontname='sans-serif', weight='semibold')
+    title_text = (
+        f'EPL: The race for European Competitions'
+        f'\n{title_pos[0]} to {title_pos[1]} as of {cur_day}'
+        )
+
+    plt.title(title_text, size=17, fontname='sans-serif', weight='semibold')
+    plt.xlabel("Teams in order of highest possible points total",
+               labelpad=15, size=17, fontname='sans-serif', weight='semibold')
+    plt.ylabel("Points and remaining fixures in chronological order",
+                labelpad=30, size=17, fontname='sans-serif', weight='semibold')
     plt.xticks(rotation=60)
 
 
@@ -340,7 +372,8 @@ def generate_table(pos_one, pos_two):
     teams_all = teams_all.reset_index()
     ucl_required = teams_all['max_points'][4]
     plt.axhline(y=ucl_required, color='#00004b', linestyle=(0, (5, 5)) )
-    plt.text(ax_width, ucl_required+0.25, f"Above {ucl_required} points guarantees UCL", color='#00004b', ha='right', weight='semibold', size='medium')
+    plt.text(ax_width, ucl_required+0.25, f"Above {ucl_required} points guarantees UCL",
+             color='#00004b', ha='center', weight='semibold', size='medium')
 
     # offset the line and label if overlapping UCL and UEL
     uel_required = teams_all['max_points'][5]
@@ -354,7 +387,8 @@ def generate_table(pos_one, pos_two):
         alig = 'baseline'
 
     plt.axhline(y=uel_required, color='#ff6900', linestyle=stylo_uel)
-    plt.text(ax_width, uel_required+offset, f"Above {uel_required} points guarantees UEL", color='#ff6900', ha='right', weight='semibold', size='medium', va=alig) #va='top', 
+    plt.text(ax_width, uel_required+offset, f"Above {uel_required} points guarantees UEL",
+             color='#ff6900', ha='center', weight='semibold', size='medium', va=alig) #va='top',
 
 
     # Axis modifications
@@ -369,8 +403,7 @@ def generate_table(pos_one, pos_two):
     date_time = datetime.today().strftime('%d-%m-%y %H.%M')
     title_name = f'History/PL Europe Race {date_time}.png'
     plt.savefig(title_name, bbox_inches='tight', pad_inches=0.5, dpi=300)
-    plt.show()
+    print('Done')
+    # plt.show()
 
 generate_table(1, 9)
-
-
