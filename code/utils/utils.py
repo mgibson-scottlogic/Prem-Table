38a629c477bf1c569e3f8dea111e530ce216161a
Matthew <mgibson@scottlogic.com>
2025-09-23 10:45:57 +0100

    Refactor utils to improve readability

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -2,23 +2,25 @@
 
 def get_current_gameweek(fixtures):
     """A function to calculate the current gameweek, for use in the title"""
-    # get all values where the fixture is finished, then get the most recent finished one
+    fixtures = fixtures.sort_values("kickoff_time").reset_index(drop=True)
+
     finished_fixtures = fixtures[fixtures.finished_provisional]
-    if not finished_fixtures.empty:
-        most_recent = fixtures[fixtures.finished_provisional].iloc[-1]
-    else:
+    if finished_fixtures.empty:
         return 'Start of Season'
-    # get the next fixture after the most recent finished fixture
-    try:
-        next_fix = fixtures.iloc[most_recent.name + 1]
-    except IndexError: # if season is over, there is no next fixture
+
+    most_recent = finished_fixtures.iloc[-1]
+
+    upcoming_fixtures = fixtures.loc[most_recent.name + 1 :]
+    next_fixture = upcoming_fixtures[upcoming_fixtures.status != 'CANCELLED'].head(1)
+
+    if next_fixture.empty:
         return 'End of Season'
 
-    # if the next fixture is: in the same gameweek as the most recent, or has started, it is midweek
-    if (next_fix.event == most_recent.event) or (next_fix.started):
-        return f'Mid GW{int(next_fix.event)}'
-    # if the next fixture is in the next gameweek, the week is over
-    if next_fix.event > most_recent.event:
-        return f'End of GW{int(most_recent.event)}'
-    # if next fixture is in the past, error safely and return blank
+    next_fixture = next_fixture.iloc[0]
+
+    # event is gameweek
+    if (next_fixture.event == most_recent.event) or (next_fixture.started):
+        return f"Mid GW{int(next_fixture.event)}"
+    if next_fixture.event > most_recent.event:
+        return f"End of GW{int(most_recent.event)}"
     return ""
